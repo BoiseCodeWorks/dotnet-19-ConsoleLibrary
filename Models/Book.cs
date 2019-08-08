ee2d63e67a3124f0fd7e2ee4c095feb1fde2d78a
Darryl Kilzer <darrylkilzer@gmail.com>
2019-08-07 18:30:03 -0600

    Josh

---
+++ Models/Book.cs
@@ -0,0 +1,17 @@
+namespace ConsoleLibrary.Models
+{
+  public class Book
+  {
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Pages { get; set; }
+    public bool Available { get; set; }
+    public Book(string title, string author, int pages, bool available = true)
+    {
+      Title = title;
+      Author = author;
+      Pages = pages;
+      Available = available;
+    }
+  }
+}
