05f09b8015e74b5f397be15f3b92e6340a908468
Darryl Kilzer <darrylkilzer@gmail.com>
2019-08-07 19:32:30 -0600

    Rommel

--- App.cs
+++ App.cs
@@ -1,3 +1,4 @@
+using System;
 using ConsoleLibrary.Models;
 
 namespace ConsoleLibrary
@@ -14,14 +15,26 @@ namespace ConsoleLibrary
       Book patsy = new Book("Patsy", "Nicole Benn-Dennis", 250);
 
       //TODO add those books to your library
+      Library.Books.Add(hp);
+      Library.Books.Add(dune);
+      Library.Books.Add(patsy);
     }
 
     public void Run()
     {
       //TODO write our application
+      Console.WriteLine("Welcome to the Library!");
       //1. we need a greeting and menu navigation
+
       //2. must be able to view, checkout, and return books
       //[STRETCH GOAL] allow the user to create instances of books 
+    }
+    public void DisplayMenu()
+    {
+      Console.WriteLine("1. View books");
+      Console.WriteLine("2. Return a book");
+      Console.WriteLine("3. Leave the library");
+      Console.WriteLine("Please enter a number");
     }
 
     // NOTE Our constructor method runs when an instance of an App is created and the logic within assigns a new instance of a Library to the property Library on our app
